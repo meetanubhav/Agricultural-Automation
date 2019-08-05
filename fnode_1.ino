04401ec7781a90c19ecc53d963e34b3bbbaf934d
meetanubhav <anubhavgupta.gupta28@gmail.com>
2019-08-05 20:12:08 +0530

    Node 1 basic code added

--- fnode_1.ino
+++ fnode_1.ino
@@ -1,11 +1,36 @@
 #include  <ESP8266WiFi.h>
+#include <dht.h>
+
+#define dht_apin A0 
 
 #define WIFI_SSID "ProjectAI" 
 #define WIFI_PASSWORD "project@sil123" 
+dht DHT;
+void setup(){
+	Serial.begin(9600);
+  	Serial.print("Wifi connecting to ");
+ 	Serial.println(WIFI_SSID);
+	Serial.println("Wifi Connected Success!");
+  	WiFi.begin (WIFI_SSID, WIFI_PASSWORD);
+  	while (WiFi.status() != WL_CONNECTED) 
+  	{
+     delay(500);
+     Serial.print(".");
+   }
 
-void setup(){}
+}
 void loop(){
+DHT.read11(dht_apin);
+    
+    Serial.print("Current humidity = ");
+    Serial.print(DHT.humidity);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(DHT.temperature); 
+    Serial.println("C  ");
+    
+    delay(5000);//Wait 5 seconds before accessing sensor again.
 
-http://192.168.0.100/projectai/first.php?x=2
+	//http://192.168.0.100/projectai/first.php?x=2
 	
 }

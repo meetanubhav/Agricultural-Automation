71bae72e866178b34ddb8320d67a828994e56b21
meetanubhav <anubhavgupta.gupta28@gmail.com>
2019-08-05 18:34:26 +0530

    Node 1 basic code lines inititated

---
+++ fnode_1.ino
@@ -0,0 +1,7 @@
+#include  <ESP8266WiFi.h>
+
+#define WIFI_SSID "monster" // Change the name of your WIFI
+#define WIFI_PASSWORD "12345678" // Change the password of your WIFI
+
+void setup(){}
+void loop(){}

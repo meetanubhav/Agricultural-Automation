96768f90119f7c2d4dcb75cc2e4cb09dc4be79eb
meetanubhav <anubhavgupta.gupta28@gmail.com>
2019-08-07 23:38:58 +0530

    added functions for each button

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
-import db
+# import db
 def showtext():
     # msg = messagebox.showinfo("Say Hello", "Hello World")
     ui_obj=ui_main()
@@ -15,19 +15,17 @@ class ui_main:
     lblinfo.grid(row=0, column=2, pady=30, padx=30)
     c="red"
     def fun_btn1(window,c):
-        btn1 = tk.Button(window, text="B1",fg="black",background=c,command=showtext)
+        btn1 = tk.Button(window, text="B1",fg="white",background=c,command=showtext)
         btn1.grid(row=1, column=1, rowspan=2, pady=30, padx=30)
         btn1.config(height=2, width=6)
-    fun_btn1(window,c)
-    def showtext1(self):
-        print()
-        self.fun_btn1(self.window,"black")
-    btn2= tk.Button(window, text="B2",fg="green",command=showtext)
-    btn2.config(height=2, width=6)
-    btn2.grid(row=2, column=1, rowspan=2, pady=30, padx=30)
-    btn3 = tk.Button(window, text="B3",fg="green",command=showtext)
-    btn3.config(height=2, width=6)
-    btn3.grid(row=3, column=1, rowspan=2,  pady=30, padx=30)
+    def fun_btn2(window,c):
+      btn2= tk.Button(window, text="B2",fg="white",background=c,command=showtext)
+      btn2.config(height=2, width=6)
+      btn2.grid(row=2, column=1, rowspan=2, pady=30, padx=30)
+    def fun_btn3(window,c):
+      btn3 = tk.Button(window, text="B3",fg="white",background=c,command=showtext)
+      btn3.config(height=2, width=6)
+      btn3.grid(row=3, column=1, rowspan=2,  pady=30, padx=30)
     x=1.23
     y=2.2
     z=3.2
@@ -37,6 +35,9 @@ class ui_main:
     b1.grid(row=2, column=2, rowspan=2, pady=60, padx=60)
     c1 = tk.Label(window, text=z)
     c1.grid(row=3, column=2, rowspan=2, pady=120, padx=60)
+    fun_btn1(window,c)
+    fun_btn2(window,c)
+    fun_btn3(window,c)
     window.mainloop()
 
 if __name__ == "__main__":

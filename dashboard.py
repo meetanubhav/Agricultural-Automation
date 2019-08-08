910a206c86a6934fa83addbebb501a213c0a8688
meetanubhav <anubhavgupta.gupta28@gmail.com>
2019-08-08 17:26:41 +0530

    home screen functioned

--- dashboard.py
+++ dashboard.py
@@ -1,28 +1,28 @@
 import tkinter as tk
+from tkinter import *
 from tkinter import messagebox
 # import db
 def showtext():
     # msg = messagebox.showinfo("Say Hello", "Hello World")
     # ui_obj=ui_main()
     # ui_obj.showtext1()
-    window.destroy()
-
-class welcome_scr:
-  def __init__():
-    print("inside screen 1")
-  global scr
-  scr = tk.Tk()
-  scr.mainloop()
+    try:
+      window.destroy()
+    except:
+      scr.destroy()
+      class2=ui_main()
+      class2.create_ui()
 
 class ui_main:
-  def __init__():
+  def __init__(self):
     print("do nothing")
-  def create_ui():
+    # create_ui()
+  def create_ui(self):
     global window
     window = tk.Tk()
     window.title("Agro")
     frame = tk.Frame(window)
-    window.geometry('800x800')
+    window.geometry('1204x1204')
     window.configure(background="#45B39D")
     lblinfo = tk.Label(window, font=('aria', 30, 'bold'), text="Agrosight", fg="steel blue",  bd=10, anchor='center')
     lblinfo.grid(row=0, column=2, pady=30, padx=30)
@@ -61,6 +61,26 @@ class ui_main:
     label3(window,z)
     window.mainloop()
 
+class welcome_scr:
+  def __init__(self):
+    print("inside screen 1")
+  global scr
+  scr = tk.Tk()
+  scr.title("Welcome to AgroFarms")
+  scr.geometry('800x800')
+  C = Canvas(scr, bg="blue", height=250, width=300)
+  filename = PhotoImage(file = "images/homebg.png")
+  background_label = Label(scr, image=filename)
+  background_label.place(x=0, y=0, relwidth=1, relheight=1)
+  frame = Frame(scr)
+  frame.pack()
+  button1 = tk.Button(scr, text="Next ➔",fg="white",background="#2874A6",command=showtext)
+  # btn1.grid(row=15, column=15, rowspan=2,pady=30,padx=30)
+  # btn1.config(height=2, width=10)
+  button1.pack(side=BOTTOM)
+  scr.mainloop()
+    
+
 if __name__ == "__main__":
     # ui_main()
-    welcome_scr()
+    class1=welcome_scr()

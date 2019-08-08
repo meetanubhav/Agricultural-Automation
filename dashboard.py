7dc226f15d0c379299bc33d0cac712a53922f263
meetanubhav <anubhavgupta.gupta28@gmail.com>
2019-08-08 16:28:25 +0530

    creating a welcome screen

--- dashboard.py
+++ dashboard.py
@@ -1,35 +1,29 @@
 import tkinter as tk
 from tkinter import messagebox
-import random
 # import db
-def welcome_screen():
-	global welscr
-	welscr = tk.Tk()
-	# msg = messagebox.showinfo("Farmer's Friend","Welcome")
-	frame = tk.Frame(welscr)
-	welscr.geometry('800x800')
-	welscr.configure(background="green")
-	btn1 = tk.Button(welscr, text="B1",fg="white",background="black",command=showtext)
-	btn1.grid(row=1, column=1, rowspan=2, pady=30, padx=30)
-	btn1.config(height=2, width=6)
-	welscr.mainloop()
-	# print("hello")
 def showtext():
+    # msg = messagebox.showinfo("Say Hello", "Hello World")
     # ui_obj=ui_main()
     # ui_obj.showtext1()
-    # if(yy==2):
-    try:
-    	window.destroy()
-    except:
-    	welscr.destroy()
-    	# ui_main
-    print("executed")
+    window.destroy()
+
+class welcome_scr:
+  def __init__():
+    print("inside screen 1")
+  global scr
+  scr = tk.Tk()
+  scr.mainloop()
+
 class ui_main:
-    global window;window = tk.Tk()
+  def __init__():
+    print("do nothing")
+  def create_ui():
+    global window
+    window = tk.Tk()
     window.title("Agro")
     frame = tk.Frame(window)
     window.geometry('800x800')
-    window.configure(background="green")
+    window.configure(background="#45B39D")
     lblinfo = tk.Label(window, font=('aria', 30, 'bold'), text="Agrosight", fg="steel blue",  bd=10, anchor='center')
     lblinfo.grid(row=0, column=2, pady=30, padx=30)
     c="red"
@@ -45,9 +39,9 @@ class ui_main:
       btn3 = tk.Button(window, text="B3",fg="white",background=c,command=showtext)
       btn3.config(height=2, width=6)
       btn3.grid(row=3, column=1, rowspan=2,  pady=30, padx=30)
-    x=random.randrange(20,50,3)
-    y=random.randrange(20,50,3)
-    z=random.randrange(20,50,3)
+    x=1.23
+    y=2.2
+    z=3.2
     def label1(window,x):
       a1 = tk.Label(window,  text=x)
       a1.grid(row=1, column=2, rowspan=2, pady=30, padx=60)
@@ -68,4 +62,5 @@ class ui_main:
     window.mainloop()
 
 if __name__ == "__main__":
-    welcome_screen()
+    # ui_main()
+    welcome_scr()
